6e150ac27f7190b7637a5cb2c2b536af0c3653d6
Abdul Basit <basit.c1@gmail.com>
2020-07-31 23:10:44 +0530

    Final Commit

---
+++ config/keys.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = "mongodb+srv://basit:basitmanna@cluster0.sjei4.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex:true
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB;
